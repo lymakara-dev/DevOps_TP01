a04e195cbe26682f52ce719bd97d3d02a47dec33
lymakara-dev <ly.makara.personal@gmail.com>
2025-04-02 08:36:56 +0700

    first commit"

---
+++ src/main/java/itc/gic/i4b/App.java
@@ -0,0 +1,17 @@
+package itc.gic.i4b;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
